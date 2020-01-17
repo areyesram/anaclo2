9777b73d707dfdfdba3ef34516a08fe8567faf78
Alejandro Reyes <areyram@abaton.com.mx>
2020-01-17 11:45:40 -0600

    resharped

--- Form1.cs
+++ Form1.cs
@@ -1,52 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Windows.Forms;
 
 namespace anaclo
 {
-    public partial class Form1 : Form
+    public sealed partial class Form1 : Form
     {
+        private IContainer components;
+        private Timer _timer;
+
         private int _radius;
         private Point _origin;
-        private Dictionary<string, Pen> _pens = new Dictionary<string, Pen>();
+        private readonly Dictionary<string, Pen> _pens = new Dictionary<string, Pen>();
         private Rectangle _circleBounds;
 
         public Form1()
         {
             InitializeComponent();
-            Paint += new PaintEventHandler(DrawClock);
+            Paint += (o, e) => DrawClock(e.Graphics);
             WinApi.MakeDraggable(this);
             SetStyle(ControlStyles.SupportsTransparentBackColor, true);
-            this.BackColor = Color.Lime;
-            this.TransparencyKey = Color.Lime;
+            BackColor = Color.Lime;
+            TransparencyKey = Color.Lime;
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void DrawClock(Graphics g)
         {
-            this._radius = ClientSize.Width / 2;
-            this._origin = new Point(_radius, _radius);
-            _pens.Add("t", Pens.Gray);
-            _pens.Add("s", Pens.Red);
-            _pens.Add("m", new Pen(Brushes.Black, 3));
-            _pens.Add("h", new Pen(Brushes.Black, 6));
-            var r = 5;
-            _circleBounds = new Rectangle(_origin.X - 5, _origin.Y - r, r * 2, r * 2);
-            Invalidate();
-        }
-
-        private void timer1_Tick(object sender, EventArgs e)
-        {
-            Invalidate();
-        }
-
-        private void DrawClock(object sender, PaintEventArgs e)
-        {
-            var g = e.Graphics;
             g.SmoothingMode = SmoothingMode.HighQuality;
 
-            e.Graphics.FillEllipse(Brushes.Gainsboro, this.ClientRectangle);
+            g.FillEllipse(Brushes.Gainsboro, this.ClientRectangle);
 
             for (var m = 0; m < 60; m++)
                 g.DrawLine(_pens["t"], GetCoords(m, 60, m % 5 == 0 ? 0.9 : 0.98), GetCoords(m, 60));
@@ -58,12 +43,46 @@ namespace anaclo
             g.FillEllipse(Brushes.Black, _circleBounds);
         }
 
-        private Point GetCoords(int value, int max, double scale = 1)
+        private Point GetCoords(int value, int max, double scale = 1.0)
         {
             var angle = value * 2 * Math.PI / max - 2 * Math.PI / 4;
             var x = (int)(Math.Cos(angle) * _radius * scale) + _radius;
             var y = (int)(Math.Sin(angle) * _radius * scale) + _radius;
             return new Point(x, y);
+        }
+
+        private void InitializeComponent()
+        {
+            components = new Container();
+            SuspendLayout();
+
+            _timer = new Timer(components) { Enabled = true, Interval = 1000 };
+            _timer.Tick += (sender, e) => Invalidate();
+
+            BackColor = SystemColors.ControlDarkDark;
+            ClientSize = new Size(200, 200);
+            DoubleBuffered = true;
+            FormBorderStyle = FormBorderStyle.None;
+            Load += (o, e) =>
+            {
+                this._radius = ClientSize.Width / 2;
+                this._origin = new Point(_radius, _radius);
+                _pens.Add("t", Pens.Gray);
+                _pens.Add("s", Pens.Red);
+                _pens.Add("m", new Pen(Brushes.Black, 3));
+                _pens.Add("h", new Pen(Brushes.Black, 6));
+                const int r = 5;
+                _circleBounds = new Rectangle(_origin.X - 5, _origin.Y - r, r * 2, r * 2);
+                Invalidate();
+            };
+            ResumeLayout(false);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+                components?.Dispose();
+            base.Dispose(disposing);
         }
     }
 }
--- WinApi.cs
+++ WinApi.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows.Forms;
+// ReSharper disable InconsistentNaming
 
 namespace anaclo
 {
